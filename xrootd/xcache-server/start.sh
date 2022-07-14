08b7797db8980af8816bbf9370de248889521513
Andres Moya <andres.moya.i@gmail.com>
2022-07-14 14:22:38 -0700

    adjust logic what service to start

--- xrootd/xcache-server/start.sh
+++ xrootd/xcache-server/start.sh
@@ -1,7 +1,7 @@
 source ../../environment
 python3 nodeScan.py
-if [[ $HOSTNAME == *"nvme"* ]]; then
-  docker compose up --detach xcache-server
+if [[ "$HOSTNAME" =~ ^xrd-cache- ]]; then
+  docker compose up --detach xcache-server xrootd-clustered-server
 else
-  docker compose up --detach
+  docker compose up --detach xcache-server
 fi
