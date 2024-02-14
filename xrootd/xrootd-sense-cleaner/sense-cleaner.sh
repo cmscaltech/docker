e0352f80590c2c35d7b26732f060226cdb7920a3
Justas Balcas <juztas@gmail.com>
2024-02-14 11:42:09 -0600

    sense cleaner update

--- xrootd/xrootd-sense-cleaner/sense-cleaner.sh
+++ xrootd/xrootd-sense-cleaner/sense-cleaner.sh
@@ -4,12 +4,12 @@
 while :
 do
     if test "${SENSE_CLEANUP+x}"; then
-        echo "[Cleaner] Cleaning UP /storage/cms/store/temp/* files older than 5 minute files"
-        find /storage/cms/store/temp/* -type f -mmin +5 -exec rm -f {} \;
+        echo "[Cleaner] Cleaning UP /storage/cms/store/temp/* files older than 1 minute files"
+        find /storage/cms/store/temp/* -type f -mmin +1 -exec rm -f {} \;
     else
         echo "[Cleaner] No SENSE_CLEANUP Flag set. Exit and will not do anything"
         exit 0
     fi
-    echo "[Cleaner] Sleep 1 minute before next loop"
-    sleep 60
+    echo "[Cleaner] Sleep 5 seconds before next loop"
+    sleep 5
 done
