5e41c39774373ccff2e313e7610f9a9cbaec415d
Andres Moya <andres.moya.i@gmail.com>
2025-08-27 17:56:32 -0700

    removing all trust chain certs from xrootd file

--- xrootd/xrootd-stageout-server/config/default/opt/postStart.sh
+++ xrootd/xrootd-stageout-server/config/default/opt/postStart.sh
@@ -6,14 +6,8 @@ mkdir -p /etc/grid-security/xrootd/
 
 cp /etc/grid-security/xrootdkey.pem /etc/grid-security/xrootd/
 
-# check if 4th cert is COMODO SHA1 signed root
-openssl storeutl --noout --text --certs /etc/grid-security/xrootdcert.pem | grep "Subject: C=GB, ST=Greater Manchester, L=Salford, O=Comodo CA Limited, CN=AAA Certificate Services"
-if [ $? -eq 0 ]; then
-  # if AAA Certificate Services remove it (keeps only 3)
-  ( openssl x509; openssl x509 ; openssl x509 ) < /etc/grid-security/xrootdcert.pem > /etc/grid-security/xrootd/xrootdcert.pem
-else
-  cp /etc/grid-security/xrootdcert.pem /etc/grid-security/xrootd/xrootdcert.pem
-fi
+# remove trust chain (keep only 1st cert from the bundle)
+openssl x509 -in /etc/grid-security/xrootdcert.pem -out /etc/grid-security/xrootd/xrootdcert.pem
 
 chown -R xrootd:xrootd /etc/grid-security/xrootd/
 
